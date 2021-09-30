c9ecd033002eddd6d0e36fa1641b7a7ef081815b
Simon Trapp <56922813+simontrapp@users.noreply.github.com>
2021-09-30 16:35:16 +0200

    customized bucket name
    S3 buckets need to be globally unique. The default name may be taken by some other user!

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ export REGION=$1
 export STAGE=$2
 export ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text`
 echo $ACCOUNT_ID
+
+# change default bucket name
+DOMAIN="hello-retail-${ACCOUNT_ID}.biz"
+sed -i "s|hello-retail.biz|${DOMAIN}|g" deploy.sh
+sed -i "s|hello-retail.biz|${DOMAIN}|g" private.yml
+sed -i "s|hello-retail.biz|${DOMAIN}|g" product-photos/productPhotos.yml
+
 npm run root:install:all
